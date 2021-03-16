b10c36bac76aaae2b7d8fc885439dd4728bc100d
notdansls <notdansls@gmail.com>
2021-03-16 17:23:13 +0000

    First functional version, stepped to v0.5. Want to add some functionality before v1.0

--- shutdownVMs.sh
+++ shutdownVMs.sh
@@ -4,13 +4,17 @@
 # has been initiated by the host system
 
 # Change log
-# +------+------------+--------------------------------------------------------------------+-------+
-# | ver  |    Date    | Description                                                        | Auth  |
-# +------+------------+--------------------------------------------------------------------+-------+
-# | 0.01 | 2021-03-14 | Initial Version                                                    |       |
-# |      |            |   * Testing stage                                                  | ndsls |
-# |      |            |   * take output from virsh list to array                           |       |
-# +------+------------+--------------------------------------------------------------------+-------+
+# +------+------------+--------------------------------------------------------------------+-----------+
+# | ver  |    Date    | Description                                                        |   Auth    |
+# +------+------------+--------------------------------------------------------------------+-----------+
+# | 0.01 | 2021-03-14 | Initial Version                                                    |           |
+# |      |            |   * Testing stage                                                  | notdansls |
+# |      |            |   * take output from virsh list to array                           |           |
+# +------+------------+--------------------------------------------------------------------+-----------+
+# | 0.50 | 2021-03-14 | First working version                                              |           |
+# |      |            |   * Created functions to simplify code                             | notdansls |
+# |      |            |   * removed echo debugging                                         |           |
+# +------+------------+--------------------------------------------------------------------+-----------+
 
 # Functions
 # ---------
@@ -20,11 +24,12 @@ listGuests(){
 	activeGuests=( $(sudo virsh list --name) )
 	x=1
 	# Check if there are any active virtual machines running
-	if $activeGuests[@] > 0 then
-		for i in "$activeGuests[@]"; do
+	if [ $activeGuests[@] > 0 ]; then
+		for i in "${activeGuests[@]}"; do
 			killGuest $i
-			if $? = 0 then return 0
+			x=$(($x + 1))			
 		done
+		return 0
 	else
 		return 1
 	fi
@@ -35,13 +40,19 @@ killGuest(){
 	# take vmname
 	# preface it with virsh shutdown xxx
 	# return true
-	echo $1
+	sudo virsh shutdown $1
         return 0
 }
 
-if $listGuests = 0 then
-	echo Guests shutting down
-else
-	echo no guests found
-fi
+# Code
+# ----
 
+listGuests
+
+#echo $?
+
+#if [ -f $? > 0 ]; then
+#	echo Guests shutting down
+#else
+#	echo no guests found
+#fi
